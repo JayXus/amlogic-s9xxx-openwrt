ce3108af322019a63727a40c8727bbd16a074c03
JayXus <9576123+JayXus@users.noreply.github.com>
2024-03-26 14:20:46 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -10,6 +10,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
